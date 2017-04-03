fa17c5a42e6c429866178c2699752f3fb577c146
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-04-03 17:00:33 +0200

    Add js from treehouse

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 /**
  * Created by vagrant on 4/3/17.
  */
+//Question function
+
+//Construction function that takes in text of the question, array of choices, and the correct answer
+
+function Question (text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+//Create a method on the question that checks if the users choice is the correct answer
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+};
